2e4e9c34046b4c0687a2dad1b5c97cda31782bb1
Vyacheslav-Fialko <vyacheslav1@ukr.net>
2021-03-06 00:32:52 +0200

    feat: add code to dateSample function

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,25 @@
+
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const regExp = new RegExp("^\\d*(\\.\\d+)?$");
+  if (!sampleActivity){
+    return false;
+  }
+  if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+  if (!regExp.test(sampleActivity)){
+    return false;
+  }
+  if (+sampleActivity <= 0 || +sampleActivity > 15) {
+    return false;
+  }
+  
+  const NATURAL_LOGARITHM_OF_TWO = 0.693;
+  const RATE_REACTION = NATURAL_LOGARITHM_OF_TWO / HALF_LIFE_PERIOD;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / RATE_REACTION);
 };
